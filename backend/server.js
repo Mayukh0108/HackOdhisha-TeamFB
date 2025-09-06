c04e19a6496082edf27bbfe8c8e0d4b04d7b73c1
Mayukh <mayukhm796@gmail.com>
2025-09-06 23:49:57 +0530

    big fix

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-      origin: ['http://localhost:8080'],
+      origin: ['*'],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
